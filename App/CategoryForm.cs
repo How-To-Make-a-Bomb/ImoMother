dfc5960759d0ba26baa1fa25b0ef147995a60ed1
MakiAT <161034995+MakiAT12@users.noreply.github.com>
2025-02-25 18:51:12 +0800

    csac

---
+++ App/CategoryForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GroceryAppWindowsForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            dairyCtrl.Hide();
+            bakeryCtrl.Hide();
+            snacksCtrl.Hide();
+            bevCtrl.Hide();
+            grains1.Hide();
+            produce1.Hide();
+            receiptUserControl1.Hide();
+
+
+        }
+        private void snacksBtn_Click(object sender, EventArgs e)
+        {
+            dairyCtrl.Hide();
+            snacksCtrl.Show();
+            bakeryCtrl.Hide();
+            bevCtrl.Hide();
+            grains1.Hide();
+            produce1.Hide();
+            receiptUserControl1.Hide();
+
+        }
+        private void dairyBtn_Click(object sender, EventArgs e)
+        {
+            dairyCtrl.Show();
+            bakeryCtrl.Hide();
+            snacksCtrl.Hide();
+            bevCtrl.Hide();
+            receiptUserControl1.Hide();
+
+            grains1.Hide();
+            produce1.Hide();
+        }
+        private void bakeryBtn_Click(object sender, EventArgs e)
+        {
+            dairyCtrl.Hide();
+            bakeryCtrl.Show();
+            snacksCtrl.Hide();
+            bevCtrl.Hide();
+            grains1.Hide();
+            receiptUserControl1.Hide();
+
+            produce1.Hide();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void beveragesBtn_Click(object sender, EventArgs e)
+        {
+            dairyCtrl.Hide();
+            bakeryCtrl.Hide();
+            snacksCtrl.Hide();
+            bevCtrl.Show();
+            grains1.Hide();
+            produce1.Hide();
+            receiptUserControl1.Hide();
+
+        }
+
+        private void grainsBtn_Click(object sender, EventArgs e)
+        {
+            dairyCtrl.Hide();
+            bakeryCtrl.Hide();
+            snacksCtrl.Hide();
+            bevCtrl.Hide();
+            grains1.Show();
+            produce1.Hide();
+            receiptUserControl1.Hide();
+
+        }
+
+        private void produceBtn_Click(object sender, EventArgs e)
+        {
+            dairyCtrl.Hide();
+            bakeryCtrl.Hide();
+            snacksCtrl.Hide();
+            bevCtrl.Hide();
+            grains1.Hide();
+            produce1.Show();
+            receiptUserControl1.Hide();
+
+        }
+
+        private void bckLbl_Click(object sender, EventArgs e)
+        {
+            MainMenuForm mainMnu = new MainMenuForm();
+            mainMnu.Show();
+            this.Hide();   
+        }
+
+        private void cartBtn_Click(object sender, EventArgs e)
+        {
+            receiptUserControl1.Show();
+
+            dairyCtrl.Hide();
+            bakeryCtrl.Hide();
+            snacksCtrl.Hide();
+            bevCtrl.Hide();
+            grains1.Hide();
+            produce1.Hide();
+        }
+    }
+}
---
+++ App/dairyUserControl.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GroceryAppWindowsForm
+{
+    public partial class dairyUserControl : UserControl
+    {
+        // Initialization
+        private int milkCount = 0;
+        private int cheeseCount = 0;
+        private int yogurtCount = 0;
+        public dairyUserControl()
+        {
+            InitializeComponent();
+            UpdateTextBoxes();
+            AttachKeyPressEvents(); 
+        }
+        private void AttachKeyPressEvents()
+        {
+            milkTxtBx.KeyPress += ValidateInput;
+            cheeseTxtBox.KeyPress += ValidateInput;
+            yogurtTxtBox.KeyPress += ValidateInput;
+        }
+
+        // Cannot Input letters and symbols in the TextBox
+        private void ValidateInput(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; 
+            }
+        }
+
+        // Method to update textboxes
+        private void UpdateTextBoxes()
+        {
+            milkTxtBx.Text = milkCount.ToString();
+            cheeseTxtBox.Text = cheeseCount.ToString();
+            yogurtTxtBox.Text = yogurtCount.ToString();
+        }
+
+        // Update the Count
+        private void UpdateCount(TextBox textBox, ref int count, bool increment)
+        {
+            if (int.TryParse(textBox.Text, out int userValue))
+            {
+                count = userValue;
+            }
+
+            if (increment)
+                count++;
+            else if (count > 0)
+                count--;
+
+            textBox.Text = count.ToString();
+        }
+
+        // Buttons
+        private void milkInc_Click(object sender, EventArgs e) => UpdateCount(milkTxtBx, ref milkCount, true);
+        private void milkDec_Click(object sender, EventArgs e) => UpdateCount(milkTxtBx, ref milkCount, false);
+
+        private void cheeseInc_Click(object sender, EventArgs e) => UpdateCount(cheeseTxtBox, ref cheeseCount, true);
+        private void cheeseDec_Click(object sender, EventArgs e) => UpdateCount(cheeseTxtBox, ref cheeseCount, false);
+
+        private void yogurtInc_Click(object sender, EventArgs e) => UpdateCount(yogurtTxtBox, ref yogurtCount, true);
+        private void yogurtDec_Click(object sender, EventArgs e) => UpdateCount(yogurtTxtBox, ref yogurtCount, false);
+
+        private void milkAddBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cheeseAddBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yogurtAddBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
